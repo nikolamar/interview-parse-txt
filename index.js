ea90fb935720bf355698ced659afb71ba3638cbc
Nikola Marjanovic <nikolamarjanovic@Nikolas-MacBook-Pro.local>
2017-07-31 18:28:10 +0200

    init

--- index.js
+++ index.js
@@ -10,16 +10,20 @@ const rows = data.split("\n")
 // remove first two rows
 rows.splice(0,2)
 
+// regex for this usecase
+const rx = /(\b[A-Z][a-z]+[\s()[a-z]+]?)(\.?\d?\.?\d+)(\s?[kg]+)(\s+)(\$)(\d?.?\d+)/
+
 const result = rows.map((val, i) => {
   // extract values into groups
-  const regexResult = (/(\b[A-Z][a-z]+[\s()[a-z]+]?)(\.?\d?\.?\d+)(\s?[kg]+)(\s+)(\$)(\d?.?\d+)/g).exec(val)
+  const regexResult = rx.exec(val)
 
   // take values from regex groups
-  let mlt = 1
   const name = regexResult[1].trim()
   const unit = regexResult[3].trim().toUpperCase()
-  if (unit === "KG") mlt = 1000
-  const weight = parseFloat(regexResult[2].trim()) * mlt
+  let weight = parseFloat(regexResult[2].trim())
+  if (unit === "KG") {
+    weight *= 1000
+  }
   const currency = regexResult[5]
   const price = parseFloat(regexResult[6].trim())
 
@@ -29,5 +33,5 @@ const result = rows.map((val, i) => {
 const sumOfWeights = result.reduce((acc, val) => acc + val.weight, 0)
 const sumOfPrices = result.reduce((acc, val) => acc + val.price, 0)
 
-console.log(`Sum of weights is ${sumOfWeights}g or ${(sumOfWeights/1000).toFixed(2)}kg and sum of prices is ${sumOfPrices.toFixed(2)}$`)
+console.log(`Sum of weights is ${(sumOfWeights/1000).toFixed(2)}kg and sum of prices is ${sumOfPrices.toFixed(2)}$`)
 
